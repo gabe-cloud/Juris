c137b30765ffe40123607c7af3d8515df3e18f56
GR <gjrzr280@gmail.com>
2022-07-21 16:45:27 -0400

    hola

--- src/database.js
+++ src/database.js
@@ -13,9 +13,7 @@ pool.getConnection((err, connection) => {
     if (err.code === 'ER_CON_COUNT_ERROR') {
       console.error('Database has to many connections');
     }
-    if (err.code === 'ECONNREFUSED') {
-      console.error('Database connection was refused');
-    }
+
   }
 
   if (connection) connection.release();
